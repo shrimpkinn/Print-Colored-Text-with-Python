f076c5a2067264bea5c95eb771583a1f5b9571e1
arquitecture <92296629+arquitecture@users.noreply.github.com>
2021-10-11 13:41:21 +0530

    Create color.py

---
+++ color.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Kishan Kaushhik "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Kishan Kaushhik")
+print(Fore.RED + Back.GREEN+ "Hi My name is Kishan Kaushhik")
