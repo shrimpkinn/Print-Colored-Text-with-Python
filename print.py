f8b8c49cac8b3213741629bb8ba502a10f8b7792
arquitecture <92296629+arquitecture@users.noreply.github.com>
2021-10-11 13:38:51 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,8 @@
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Kishan Kaushik "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Kishan Kaushik")
+print(Fore.RED + Back.GREEN+ "Hi My name is Kishan Kaushik")
